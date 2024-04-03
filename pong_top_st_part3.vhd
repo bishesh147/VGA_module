a7af21c0b42fa1e7c26527c9507be101823756f5
Bishesh Adhikari <73216601+bishesh147@users.noreply.github.com>
2024-04-03 16:03:50 -0400

    Update pong_top_st_part3.vhd

--- pong_top_st_part3.vhd
+++ pong_top_st_part3.vhd
@@ -6,7 +6,7 @@ entity pong_top_st is
         clk, reset: in std_logic;
         btn: in std_logic_vector(1 downto 0);
         hsync, vsync: out std_logic;
-        rgb_8bit: out std_logic_vector(23 downto 0);
+        rgb_top: out std_logic_vector(2 downto 0);
         vga_pixel_tick: out std_logic;
         blank: out std_logic;
         comp_sync: out std_logic
@@ -36,9 +36,9 @@ begin
         vga_pixel_tick <= p_tick;
 -- Set the high order bits of the video DAC for each
 -- of the three colors
-    rgb_8bit(7) <= rgb(0);
-    rgb_8bit(15) <= rgb(1);
-    rgb_8bit(23) <= rgb(2);
+    rgb_top(0) <= rgb(0);
+    rgb_top(1) <= rgb(1);
+    rgb_top(2) <= rgb(2);
 
 -- rgb buffer, graph_rgb is routed to the output through
 -- an output buffer -- loaded when p_tick = ?1?.
